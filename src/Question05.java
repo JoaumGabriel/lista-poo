50a1c151637b17123a6105e9535ff206bdb772a0
JoaumGabriel <joao.gbr190@gmail.com>
2022-09-09 00:26:59 -0300

    Questão 05 da Lista

---
+++ src/Question05.java
@@ -0,0 +1,44 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Question05 {
+    public static void main(String[] args) {
+        
+        /*
+         * Leia a idade e o tempo de serviço de um trabalhador e escreva se ele pode ou
+         * não se aposentar. As
+         * condições para aposentadoria são:
+         * ○ Ter pelo menos 65 anos;
+         * ○ Ou ter trabalhado pelo menos 30 anos;
+         * ○ Ou ter pelo menos 60 anos e trabalhado pelo menos 25 anos.
+         */
+
+        int year, workTime;
+
+        Scanner scan = new Scanner(System.in);
+
+        try {
+            System.out.print("Olá trabalhador!\n\nDigite a sua idade: ");
+            year = scan.nextInt();
+
+            System.out.print("Agora insira o tempo de trabalho (anos): ");
+            workTime = scan.nextInt();
+
+            if (year >= 65) {
+                System.out.println("STATUS: Pode se aposentar pela idade!");
+            } else if (workTime >= 30) {
+                System.out.println("STATUS: Pode se aposentar pelo tempo de trabalho!");
+            } else if (year >= 60 && workTime >= 25) {
+                System.out.println("STATUS: Pode se aposentar pela sua idade e pelo seu tempo de trabalho");
+            } else {
+                System.out.println(
+                        "STATUS: Você não pode se aposentar atualmente, pois não cumpre nenhum dos requisitos!");
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Entrada inválida!");
+        }
+
+        scan.close();
+
+    }
+}
