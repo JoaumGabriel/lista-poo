8668b4a5864375e62c3a69a05a9aa19fd538fcb0
JoaumGabriel <joao.gbr190@gmail.com>
2022-09-09 15:32:07 -0300

    Bug fix

--- src/Question15.java
+++ src/Question15.java
@@ -14,21 +14,25 @@ public class Question15 {
 
         int num, f1 = 0, f2 = 1, f3 = 0;
 
-        System.out.println("Digite um número positivo:");
-        num = scan.nextInt();
+        try {
+            System.out.println("Digite um número positivo:");
+            num = scan.nextInt();
 
-        if (num < 0) {
-            System.out.println("Número invalido");
-        } else {
-            System.out.println("Sequência:");
-            while (f2 <= num) {
-                f3 = f2 + f1;
-                System.out.println(" - " + f3);
-                f1 = f2;
-                f2 = f3;
+            if (num < 0) {
+                System.out.println("Número invalido");
+            } else {
+                System.out.println("Sequência:");
+                while (f2 <= num) {
+                    f3 = f2 + f1;
+                    System.out.println(" - " + f3);
+                    f1 = f2;
+                    f2 = f3;
+                }
+                System.out.println("\n");
+
             }
-            System.out.println("\n");
-
+        } catch (Exception e) {
+            System.out.println("Só é permitido a entrada de números!");
         }
 
         scan.close();
