1aeefd92e31ed5ded4962ee11b886277719a1c44
JoaumGabriel <joao.gbr190@gmail.com>
2022-09-09 00:28:54 -0300

    Questão 15 da Lista

---
+++ src/Question15.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Question15 {
+
+    public static void main(String[] args) {
+
+        /*
+         * Leia um número positivo do usuário, então, calcule e imprima a sequência
+         * Fibonacci até o primeiro
+         * número superior ao número lido.
+         */
+
+        Scanner scan = new Scanner(System.in);
+
+        int num, f1 = 0, f2 = 1, f3 = 0;
+
+        System.out.println("Digite um número positivo:");
+        num = scan.nextInt();
+
+        if (num < 0) {
+            System.out.println("Número invalido");
+        } else {
+            System.out.println("Sequência:");
+            while (f2 <= num) {
+                f3 = f2 + f1;
+                System.out.println(" - " + f3);
+                f1 = f2;
+                f2 = f3;
+            }
+            System.out.println("\n");
+
+        }
+
+        scan.close();
+
+    }
+}
