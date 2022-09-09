35a995a156c9e7e35dc602e01805997841d25be2
JoaumGabriel <joao.gbr190@gmail.com>
2022-09-09 00:28:23 -0300

    Questão 12 da Lista

---
+++ src/Question12.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Question12 {
+    public static void main(String[] args) {
+
+        /*
+         * Escreva um programa que leia um número inteiro e calcule a soma de todos os
+         * divisores desse
+         * número, com exceção dele próprio
+         */
+
+        Scanner scan = new Scanner(System.in);
+
+        int soma = 0;
+        int num;
+
+        System.out.println("Insira um número: ");
+        num = scan.nextInt();
+
+        for (int i = 1; i < num; i++) {
+            if (num % i == 0) {
+                soma += i;
+            }
+        }
+
+        System.out.println("Soma de todos os divisores da variavel: " + soma);
+
+        scan.close();
+    }
+}
