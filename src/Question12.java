1bfb6bed4a409faa05238ea760ceeb57f9b000ef
JoaumGabriel <joao.gbr190@gmail.com>
2022-09-09 15:31:44 -0300

    Bug fix

--- src/Question12.java
+++ src/Question12.java
@@ -14,16 +14,20 @@ public class Question12 {
         int soma = 0;
         int num;
 
-        System.out.println("Insira um número: ");
-        num = scan.nextInt();
+        try {
+            System.out.println("Insira um número: ");
+            num = scan.nextInt();
 
-        for (int i = 1; i < num; i++) {
-            if (num % i == 0) {
-                soma += i;
+            for (int i = 1; i < num; i++) {
+                if (num % i == 0) {
+                    soma += i;
+                }
             }
-        }
 
-        System.out.println("Soma de todos os divisores da variavel: " + soma);
+            System.out.println("Soma de todos os divisores da variavel: " + soma);
+        } catch (Exception e) {
+            System.out.println("Só é permitido a entrada de números!");
+        }
 
         scan.close();
     }
