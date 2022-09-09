b12b6423d1b85340d34dfe336e3d77dcc84bba53
JoaumGabriel <joao.gbr190@gmail.com>
2022-09-09 00:27:39 -0300

    Questão 08 da Lista

---
+++ src/Question08.java
@@ -0,0 +1,25 @@
+public class Question08 {
+    public static void main(String[] args) {
+
+        /*
+         * Chico tem 1.50 metro e cresce 2 centímetros por ano, enquanto Zé tem 1.10
+         * metros e cresce 3
+         * centímetros por ano. Escreva um programa que calcule e imprima quantos anos
+         * serão necessários para
+         * que Zé seja maior que Chico.
+         */
+        
+        double chicoWeight = 1.50;
+        double zeWeight = 1.10;
+
+        int countYears = 0;
+
+        do {
+            zeWeight += 0.03;
+            chicoWeight += 0.02;
+            countYears += 1;
+        } while (zeWeight < chicoWeight);
+
+        System.out.println("Ze precisa de " + countYears + " anos para ser mais alto que Chico");
+    }
+}
